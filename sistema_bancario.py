77429d1364933b9a5c693d927555014ec19f0a3d
Rafael Evandro <113847022+rafaevandro@users.noreply.github.com>
2023-08-04 18:10:40 -0300

    sistema_bancario.py
    Criando um Sistema Bancário com Python

---
+++ sistema_bancario.py
@@ -0,0 +1,75 @@
+# Constantes
+LIMITE_SAQUES = 3
+
+# Variáveis
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+
+# Funções
+def depositar(valor):
+    global saldo, extrato
+    if valor > 0:
+        saldo += valor
+        extrato += f"Depósito: R$ {valor:.2f}\n"
+    else:
+        print("Operação falhou! O valor informado é inválido.")
+
+def sacar(valor):
+    global saldo, extrato, numero_saques
+    excedeu_saldo = valor > saldo
+    excedeu_limite = valor > limite
+    excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+    if excedeu_saldo:
+        print("Operação falhou! Você não tem saldo suficiente.")
+    elif excedeu_limite:
+        print("Operação falhou! O valor do saque excede o limite.")
+    elif excedeu_saques:
+        print("Operação falhou! Número máximo de saques excedido.")
+    elif valor > 0:
+        saldo -= valor
+        extrato += f"Saque: R$ {valor:.2f}\n"
+        numero_saques += 1
+    else:
+        print("Operação falhou! O valor informado é inválido.")
+
+def exibir_extrato():
+    global extrato, saldo
+    print("\n================ EXTRATO ================")
+    if not extrato:
+        print("Não foram realizadas movimentações.")
+    else:
+        print(extrato)
+    print(f"\nSaldo: R$ {saldo:.2f}")
+    print("==========================================")
+
+# Menu
+menu = """
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+while True:
+    opcao = input(menu)
+
+    if opcao == "d":
+        valor = float(input("Informe o valor do depósito: "))
+        depositar(valor)
+
+    elif opcao == "s":
+        valor = float(input("Informe o valor do saque: "))
+        sacar(valor)
+
+    elif opcao == "e":
+        exibir_extrato()
+
+    elif opcao == "q":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
